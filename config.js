2e685b5510fa51ba159dae884264da4dd57fa8f3
lyc <22719973+l-yc@users.noreply.github.com>
2020-04-28 17:02:43 +0800

    moved db config to global config and improved init script

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 module.exports = {
   appName: 'nush-proctor',
   port: 8080,
-  privateKey: './testing/server.key',
-  certificate: './testing/server.crt',
-  sessionSecret: 'D9QzvjnAQODzseeqPmI2DuAsZqu0oAyHNxnVgI5OtnHc1Od3v6upOZHp7Yvgmo6'
+  privateKey: '/etc/letsencrypt/live/parangninja.sytes.net/privkey.pem',
+  certificate: '/etc/letsencrypt/live/parangninja.sytes.net/fullchain.pem',
+  sessionSecret: 'D9QzvjnAQODzseeqPmI2DuAsZqu0oAyHNxnVgI5OtnHc1Od3v6upOZHp7Yvgmo6',
+  db: {
+      url: 'mongodb://localhost/nush-proctor'
+  }
 };
