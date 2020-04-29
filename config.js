801e891275a694453c623d27748de1ed9592af21
lyc <22719973+l-yc@users.noreply.github.com>
2020-04-29 09:47:49 +0800

    organised config file and added iceservers configuration

--- config.js
+++ config.js
@@ -1,10 +1,19 @@
 module.exports = {
   appName: 'nush-proctor',
   port: 8080,
-  privateKey: '/etc/letsencrypt/live/parangninja.sytes.net/privkey.pem',
-  certificate: '/etc/letsencrypt/live/parangninja.sytes.net/fullchain.pem',
   sessionSecret: 'D9QzvjnAQODzseeqPmI2DuAsZqu0oAyHNxnVgI5OtnHc1Od3v6upOZHp7Yvgmo6',
+  tls: {
+    privateKey: '/etc/letsencrypt/live/parangninja.sytes.net/privkey.pem',
+    certificate: '/etc/letsencrypt/live/parangninja.sytes.net/fullchain.pem'
+  },
   db: {
-      url: 'mongodb://localhost/nush-proctor'
-  }
+    url: 'mongodb://localhost/nush-proctor'
+  },
+  iceServers: [
+    { urls:['stun:mystun.sytes.net:3478'] },
+    {
+      urls: ['turn:mystun.sytes.net:3478'],
+      authSecret: 'aGVsbG90b2Z1c2FyZXRoZWJlc3R0aGluZ2ludGhld29ybGQ='
+    }
+  ]
 };
