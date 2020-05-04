7c241fb1bd2ccfcb918649c69586a665d5c6a946
lyc <22719973+l-yc@users.noreply.github.com>
2020-05-04 18:54:21 +0800

    require login for students

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function(middleware) {
-  router.get('/', function(req, res, next) {
-    res.render('proctor', { title: global.config.appName });
-  });
-
   router.get('/login', function(req, res, next) {
     res.render('login', { title: global.config.appName, message: req.flash('message') });
   });
 
-  router.post('/login', middleware.passport.authenticate('login', {
+  router.post('/login', middleware.authenticate('login', {
     successRedirect: '/seer',
     failureRedirect: '/login',
     failureFlash: true
   }));
 
-  router.post('/signup', middleware.passport.authenticate('signup', {
+  router.post('/signup', middleware.authenticate('signup', {
     successRedirect: '/seer',
     failureRedirect: '/login',
     failureFlash: true
   }));
+
+  router.get('/', middleware.checkAuthenticated, function(req, res, next) {
+    res.render('proctor', { title: global.config.appName });
+  });
 
   router.get('/seer', middleware.checkAuthenticated, function(req, res, next) {
     res.render('seer', { title: global.config.appName });
--- routes/middleware.js
+++ routes/middleware.js
@@ -9,11 +9,16 @@ module.exports = function(passport, acl) {
   middleware = {};
   middleware.passport = passport;
 
+  // Abstraction for logins
+  middleware.authenticate = function(req, strategy, params) {
+    passport.authenticate(req, strategy, params);
+  };
+
   middleware.checkAuthenticated = function(req, res, next) {
     //debug('Checking for authentication');
     if (req.isAuthenticated()) return next();
     res.redirect('/login');
-  }
+  };
 
   return middleware;
 };
