1c2bfb441ebbd709e4420b96dc341385e453f6fd
lyc <22719973+l-yc@users.noreply.github.com>
2020-05-10 16:11:24 +0800

    added logout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 module.exports = function(middleware) {
   router.get('/login', function(req, res, next) {
-    res.render('login', { title: global.config.appName, message: req.flash('message') });
+    res.render('login', { title: 'login', message: req.flash('message') });
   });
 
   router.post('/login', middleware.authenticate('login', {
@@ -11,6 +11,11 @@ module.exports = function(middleware) {
     failureRedirect: '/login',
     failureFlash: true
   }));
+
+  router.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/login');
+  });
 
   // TODO: disabled signups for now
   //router.post('/signup', middleware.authenticate('signup', {
@@ -23,13 +28,13 @@ module.exports = function(middleware) {
   router.use(middleware.checkAuthenticated);
 
   router.get('/', function(req, res, next) {
-    res.render('proctor', { title: global.config.appName });
+    res.render('proctor', { title: 'proctor', sessionName: global.config.appName });
   });
 
   router.get('/seer', [
     middleware.checkPermission('restricted')
   ], function(req, res, next) {
-    res.render('seer', { title: global.config.appName });
+    res.render('seer', { title: 'seer', sessionName: global.config.appName });
   });
 
   return router;
