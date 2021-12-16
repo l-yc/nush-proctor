af27f23d5abee81c09ab0ddf89ea301974ac52ca
lyc <22719973+l-yc@users.noreply.github.com>
2021-12-16 18:00:01 +0800

    update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('styles', function() {
   return gulp.src('./public/sass/**/*.scss')
